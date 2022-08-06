ac4a9cbabef140f77d9193a6fe694f79ec86e233
oceanumeric <numerical.ocean@gmail.com>
2022-08-06 19:30:04 +0000

    [update] ch0

--- ch0/ml_illustration.py
+++ ch0/ml_illustration.py
@@ -2,6 +2,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 import math
 import time
+import torch
 
 
 class MlExample:
@@ -159,15 +160,73 @@ class MlExample:
     self.L = L
     return y_pred, L 
 
-  def plot_results(self):
+  def plot_results(self, gpu=False):
       fig, ax = plt.subplots(1, 2, figsize=(14, 5))
       ax[0].plot(self.L)
       ax[0].set_title(f"The loss function with alpha {self.alpha} and Iter {self.iter}")
       ax[0].annotate(round(self.L[-1], 3), (len(self.L)-5, self.L[-1]))
-      ax[1].plot(self.x, self.y, label=r"$\sin(x)$")
-      ax[1].plot(self.x, self.pred, label=self.model_label)
+      if gpu:
+        ax[1].plot(self.x.cpu().numpy(), self.y.cpu().numpy(), label=r"$\sin(x)$")
+        ax[1].plot(self.x.cpu().numpy(), self.pred.cpu().numpy(), label=self.model_label)
+      else:
+        ax[1].plot(self.x, self.y, label=r"$\sin(x)$")
+        ax[1].plot(self.x, self.pred, label=self.model_label)
       ax[1].axhline(y=0, color='k')
       ax[1].legend()
       fig.show()
+
+  def train_with_gpu(self, learning_rate, iteration):
+    self.alpha = learning_rate
+    self.iter = iteration 
+
+    # use gpu
+    dtype = torch.float
+    device = torch.device("cuda:0")
+    # x and y 
+    self.x = torch.linspace(-math.pi, math.pi, 2000, device=device, dtype=dtype)
+    self.y = torch.sin(self.x)
+    # weights
+    a = torch.randn((), device=device, dtype=dtype)
+    b = torch.randn((), device=device, dtype=dtype)
+    c = torch.randn((), device=device, dtype=dtype)
+    d = torch.randn((), device=device, dtype=dtype)
+    e = torch.randn((), device=device, dtype=dtype)
+    f = torch.randn((), device=device, dtype=dtype)
+    # loss 
+    L = []
+    start = time.time()
+    for t in range(iteration):
+        y_pred = a + b*self.x + c*self.x**2 + d*self.x**3 + e*self.x**4 + f*self.x**5
+
+        # Compute and print loss
+        loss = (y_pred - self.y).pow(2).sum().item()
+        if t % 100 == 99:
+            L.append(loss)
+
+        grad_y_pred = 2.0 * (y_pred - self.y)
+        grad_a = grad_y_pred.sum()
+        grad_b = (grad_y_pred*self.x).sum()
+        grad_c = (grad_y_pred*self.x**2).sum()
+        grad_d = (grad_y_pred*self.x**3).sum()
+        grad_e = (grad_y_pred*self.x**4).sum()
+        grad_f = (grad_y_pred*self.x**5).sum()
+
+        # update the weight
+        a -= learning_rate * grad_a
+        b -= learning_rate * grad_b
+        c -= learning_rate * grad_c
+        d -= learning_rate * grad_d
+        e -= learning_rate * grad_e
+        f -= learning_rate * grad_f 
+
+    end = time.time()
+    es = round(end-start, 3)
+    print(f"The training time is: {es} seconds")
+    self.model = f'Result: y = {a} + {b} x + {c} x^2 + {d} x^3 + {e} x^4 + {f} x^5'
+    print(self.model)
+    self.model_label = r"$y = a+bx+cx^2+dx^3+ex^4+fx^5$"
+    self.pred = y_pred
+    self.L = L
+    return y_pred, L 
 
     
