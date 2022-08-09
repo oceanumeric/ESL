95a3b7178a5c43e288e8297bb43b590081d8f36e
oceanumeric <numerical.ocean@gmail.com>
2022-08-09 16:05:07 +0200

    [update] ch2

--- ch2/ch2_osl.py
+++ ch2/ch2_osl.py
@@ -1,11 +1,11 @@
 """
 Chapter 2: overview of supervised learning
 """
-import random 
 import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
 from itertools import filterfalse, product
+import random 
 
 
 class OverviewSL:
@@ -18,13 +18,24 @@ class OverviewSL:
     size = 100
     cov = np.identity(2)
     mk_orange = np.random.multivariate_normal([0, 1], cov, 10)
+    mk_orange = random.choice(mk_orange)
     mk_blue = np.random.multivariate_normal([1, 0], cov, 10)
-    orange = np.array()
-    blue = np.random.multivariate_normal(random.choice(mk_blue), cov/5, size)
+    mk_blue = random.choice(mk_blue)
+    orange = []
+    blue = []
+    for i in range(size):
+        orange.append(
+            np.random.multivariate_normal(mk_orange, cov/5)
+        )
+        blue.append(
+            np.random.multivariate_normal(mk_blue, cov/5)
+        )
+    orange = np.array(orange)
+    blue = np.array(blue)
 
     def __init__(self) -> None:
         self.X = np.r_[self.orange, self.blue]
-        self.Y = np.r_[np.zeros(self.size), np.ones(self.size)]
+        self.Y = np.r_[np.ones(self.size), np.zeros(self.size)]
         self.weights = None
         self.pred = None
 
@@ -46,14 +57,15 @@ class OverviewSL:
         '''
         X = np.c_[np.ones((self.X.shape[0], 1)), self.X]
         self.weights = np.linalg.inv(X.T @ X) @ X.T @ self.Y
-        self.pred = np.dot(self.weights, X)
+        self.pred = np.dot(X, self.weights)
+        print("hello michael")
         print(f"The estimated coefficents are {self.weights}")
 
     def plot_linear_model(self):
         '''
         Plot the linear model 
         '''
-        is_orange = lambda x: np.dot(self.weights, x) > 0.5  # create a filter
+        is_orange = lambda x: np.dot(np.r_[1, x], self.weights) > 0.5  # create a filter
         fig, axes = plt.subplots(1, 1, figsize=(8, 8))
         axes.scatter(self.orange[:, 0], self.orange[:, 1],
                         color='#d68904', facecolor='none', s=70)
@@ -62,11 +74,22 @@ class OverviewSL:
         xlim = axes.get_xlim()
         ylim = axes.get_ylim()
         # * unpact sequences or iterators 
-        grid = np.array(*[product(np.linspace(*xlim, 50),
+        grid = np.array([*product(np.linspace(*xlim, 50),
                                   np.linspace(*ylim, 50))])
         orange_grid = np.array([*filter(is_orange, grid)])
         blue_grid = np.array([*filterfalse(is_orange, grid)])
-        axes.plot(orange_grid, blue_grid)
+        axes.plot(orange_grid[:, 0], orange_grid[:, 1], '.',
+                  zorder = 0.001, color='orange', alpha = 0.3,
+                  scalex = False, scaley = False)
+        axes.plot(blue_grid[:, 0], blue_grid[:, 1], '.',
+                  zorder = 0.001, color='blue', alpha = 0.3,
+                  scalex = False, scaley = False)
+        # a + x1*alpha + x2*beta = Y (1/0)
+        find_y = lambda x: (0.5 - self.weights[0] - x* self.weights[1]) / self.weights[2]
+        axes.plot(xlim, [*map(find_y, xlim)], color='k', 
+                  scalex = False, scaley = False)
+
+
 
 
 
