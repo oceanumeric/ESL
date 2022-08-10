9888d35ba4804d8a95825dbd6032c1d5fbe68465
oceanumeric <numerical.ocean@gmail.com>
2022-08-10 15:34:25 +0200

    [update] ch2

--- ch2/ch2_osl.py
+++ ch2/ch2_osl.py
@@ -1,12 +1,15 @@
 """
 Chapter 2: overview of supervised learning
 """
-import random 
+import random
+from turtle import fillcolor 
 import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
-from itertools import filterfalse, product
+from itertools import filterfalse, product, combinations
 from scipy.stats import multivariate_normal
+from matplotlib.patches import Rectangle, Circle, PathPatch
+import mpl_toolkits.mplot3d.art3d as art3d
 
 
 
@@ -169,6 +172,65 @@ class OverviewSL:
                   zorder = 0.001, color='#1f6f9c', alpha = 0.3,
                   scalex = False, scaley = False)
         axes.set_title(r"Bayes Optimal Classifier $\mathbb{P}(orange) = \mathbb{P}(blue) = 1/2$")
+        
+        
+    def plot_sparsity(self):
+        mu = 0 
+        sigma = 3
+        self.one_dim = np.random.normal(mu, sigma, 100)
+        self.two_dim = np.random.multivariate_normal([0, 0],
+                                                     np.identity(2)*sigma, 100)
+        fig, axes = plt.subplots(1, 2, figsize=(12, 7))
+        axes[0].scatter(self.one_dim, [0]*self.one_dim.shape[0],
+                        facecolor='none', edgecolor='r')
+        axes[0].set_title("One dimension: not much sparsity")
+        axes[1].scatter(self.two_dim[:, 0], self.two_dim[:, 1],
+                        facecolor='none', edgecolor='g')
+        axes[1].set_title("Two dimension: some sparsity")
+
+    def plot_denstity_example(self):
+        x = np.linspace(0, 1, 5)
+        xx = np.array([*product(x, x)])
+        fig, axes = plt.subplots(1, 2, figsize=(12, 7))
+        axes[0].plot(x, [0]*x.shape[0], 'or-')
+        axes[0].set_title("Sampling of density 5 in one dimension")
+        axes[0].axis('off')
+        axes[1].scatter(xx[:,0], xx[:, 1],
+                        facecolor='g', edgecolor='g')
+        axes[1].set_title("Sampling of density 5 in"\
+                        "two dimension (more points are needed")
+
+    def plot_cube(self):
+        fig = plt.figure(figsize=(12, 7))
+        ax = fig.add_subplot(121, projection='3d')
+        r = [0, 5]
+        for s, e in combinations(np.array(list(product(r,r,r))), 2):
+            if np.sum(np.abs(s-e)) == r[1]-r[0]:
+                ax.plot3D(*zip(s,e), color="k")
+        c = [0, 1]
+        for s, e in combinations(np.array(list(product(c,c,c))), 2):
+            if np.sum(np.abs(s-e)) == c[1]-c[0]:
+                ax.plot3D(*zip(s,e), color="r")
+        ax.grid(False)
+        ax.view_init(17)
+        ax.set_title('The share of unit cube is 0.8%')
+        ax = fig.add_subplot(122)
+        x = np.linspace(0, 1, 5)
+        ax.set_xlim(0, 5)
+        ax.set_ylim(0, 5)
+        ax.set_aspect('equal', adjustable='box')
+        ax.plot(x, [0]*x.shape[0], 'k')
+        ax.plot(x, [1]*x.shape[0], 'k')
+        ax.plot([1]*x.shape[0], x, 'k')
+        x = np.linspace(0, 5, 10)
+        xx = np.array([*product(x, x)])
+        ax.scatter(xx[:,0], xx[:, 1],
+                        facecolor='none', edgecolor='b')
+        ax.set_title('The share of unit cube is 4% (it covers 4 points)')
+
+
+
+        
         
         
         
