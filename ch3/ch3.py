9b3b41d62732690fb23812640ac1c62354c980a9
oceanumeric <numerical.ocean@gmail.com>
2022-08-18 17:49:38 +0200

    [update] 3.4

--- ch3/ch3.py
+++ ch3/ch3.py
@@ -194,6 +194,44 @@ class LinearRegression:
         else:
             ax.set_title("Forward-stepwise selection with the training dataset")
             
+    @staticmethod
+    def index_tenfold(n:int) ->np.ndarray:
+        """Produce index array for tenfold CV with dataframe length n."""
+        original_indices = np.arange(n)
+        tenfold_indices = np.zeros(n)
+
+        div, mod = divmod(n, 10)
+        unit_sizes = [div for _ in range(10)]
+        for i in range(mod):
+            unit_sizes[i] += 1
+
+        for k, unit_size in enumerate(unit_sizes):
+            tenfold = np.random.choice(original_indices, unit_size,
+                                    replace=False)
+            tenfold_indices[tenfold] = k
+            original_indices = np.delete(
+                original_indices,
+                [np.argwhere(original_indices == val) for val in tenfold],
+            )
+            # print(tenfold, original_indices)
+        return tenfold_indices 
+    
+    def plot_figure_3_8(self):
+        """
+        Plot figure 3.8 
+        """
+        intercept = self.y_train.mean()  # intercept is just mean now 
+        # centering y
+        y_train_centered = (self.y_train-intercept)
+        print(intercept)
+        
+        # get sample of train and test dataset
+        train_size = self.x_train.shape[0]
+        test_size = self.x_test.shape[0]
+        var_size = self.x_train.shape[1]
+           
+    
+            
             
             
             
